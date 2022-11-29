12068ee815e4a60c3adb5dd46b878f7e465cb096
Nicolas Laporte <nicolas.laporte@mbp-mtp-nla.local>
2022-11-29 15:28:42 +0100

    feat: add a new rule to check witespace between type and jira issue

--- src/rules/tests/commitTyeAndJiraIssueShouldNotHaveSpace.test.ts
+++ src/rules/tests/commitTyeAndJiraIssueShouldNotHaveSpace.test.ts
@@ -1,10 +1,10 @@
 import commitTypeAndJiraIssueShouldNotHaveSpace from '../commitTypeAndJiraIssueShouldNotHaveSpace';
 
-describe('commitTypeEmpty, check if commit message contain valid type from angular convention', () => {
+describe('commitTypeAndJiraIssueShouldNotHaveSpace, check if commit message contain valid type from angular convention', () => {
 
     describe('should reject the commit message', () => {
 
-        it('The commit do not contain any type', () => {
+        it('The commit is separated by a witespace', () => {
             const parsed = {
                 raw: 'fix (JIR-322): my commit message',
             }
@@ -12,7 +12,7 @@ describe('commitTypeEmpty, check if commit message contain valid type from angul
             expect(commitTypeAndJiraIssueShouldNotHaveSpace(parsed)[1]).toEqual("your commit is not well formated, type and jira issue should not be separated by a witespace =>  fix(JIR-333): my commit message");
         });
 
-        it('The commit contain a valid type', () => {
+        it('The commit do not contain any issue', () => {
             const parsed = {
                 raw: 'feat: my commit message',
             }
@@ -24,7 +24,7 @@ describe('commitTypeEmpty, check if commit message contain valid type from angul
 
     describe('should validate the commit message', () => {
 
-        it('The commit contain a valid type', () => {
+        it('The commit respect the convention', () => {
             const parsed = {
                 raw: 'fix(IB-21): my commit message',
             }
